a0afe9a10d688908a0577bc4c85a40f41997f5e8
Leonardo Costa <133729353+LeoCosta17@users.noreply.github.com>
2025-01-20 22:17:25 -0300

    Game class DTO, GameRepository, GameService, GameController

---
+++ src/main/java/com/projetospring/dslist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.projetospring.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.projetospring.dslist.services.GameService;
+import com.projetospring.dslist.dto.GameMinDTO;
+import com.projetospring.dslist.entities.Game;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
---
+++ src/main/java/com/projetospring/dslist/services/GameService.java
@@ -0,0 +1,24 @@
+package com.projetospring.dslist.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.projetospring.dslist.repositories.GameRepository;
+import com.projetospring.dslist.dto.GameMinDTO;
+import com.projetospring.dslist.entities.Game;
+import org.springframework.data.repository.ListCrudRepository;
+
+@Component
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> listDto = result.stream().map(x -> new GameMinDTO(x)).collect(Collectors.toList());
+        return listDto;
+    }
+}
