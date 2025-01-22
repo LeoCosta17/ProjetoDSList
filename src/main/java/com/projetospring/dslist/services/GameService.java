cfcbce3f8b276c80b019c772dd6a415db18cc1cd
Leonardo Costa <133729353+LeoCosta17@users.noreply.github.com>
2025-01-22 14:17:46 -0300

    Game DTO, SearchGameByID

--- src/main/java/com/projetospring/dslist/services/GameService.java
+++ src/main/java/com/projetospring/dslist/services/GameService.java
@@ -5,19 +5,31 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import com.projetospring.dslist.repositories.GameRepository;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.projetospring.dslist.dto.GameDTO;
 import com.projetospring.dslist.dto.GameMinDTO;
 import com.projetospring.dslist.entities.Game;
 
-@Component
+@Service
 public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        List<GameMinDTO> listDto = result.stream().map(x -> new GameMinDTO(x)).collect(Collectors.toList());
-        return listDto;
+        return result.stream().map(x -> new GameMinDTO(x)).collect(Collectors.toList());
     }
 }
